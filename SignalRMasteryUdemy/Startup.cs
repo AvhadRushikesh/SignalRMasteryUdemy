cbff49c7a9522888dd0e79dd16a42e5b7bf67107
rushi-smb <rushi@smbios.co.in>
2023-03-21 22:55:15 +0530

    Configuring Redis BackPlane

--- SignalRMasteryUdemy/Startup.cs
+++ SignalRMasteryUdemy/Startup.cs
@@ -8,7 +8,14 @@ namespace SignalRMasteryUdemy
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR();
+            // connection string to redis - Get from configuration! 
+            var connectionString = "";
+
+            // Add StackExchange Redis helper
+            services.AddSignalR().AddStackExchangeRedis(connectionString, configure => {
+                configure.Configuration.ChannelPrefix = "signalr";
+                configure.Configuration.DefaultDatabase = 5;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
