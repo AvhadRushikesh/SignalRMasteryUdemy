b56093fcec1f5776277caaa4e66fa44832795440
rushi-smb <rushi@smbios.co.in>
2023-03-21 17:44:30 +0530

    Connection events on Server

--- SignalRMasteryUdemy/Hubs/ViewHub.cs
+++ SignalRMasteryUdemy/Hubs/ViewHub.cs
@@ -8,11 +8,22 @@ namespace SignalRMasteryUdemy.Hubs
     {
         public static int ViewCount { get; set; } = 0;
 
-        public async Task NotifyWatching()
+        public async override Task OnConnectedAsync()
         {
             ViewCount++;
 
             await this.Clients.All.SendAsync("viewCountUpdate", ViewCount);
+
+            await base.OnConnectedAsync();
         }
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            ViewCount--;
+
+            await this.Clients.All.SendAsync("viewCountUpdate", ViewCount);
+
+            await base.OnDisconnectedAsync(exception);
+        }
+
     }
 }
--- SignalRMasteryUdemy/client/index.ts
+++ SignalRMasteryUdemy/client/index.ts
@@ -4,7 +4,6 @@ var counter = document.getElementById("viewCounter");
 
 // create connection
 let connection = new signalR.HubConnectionBuilder()
-    .withAutomaticReconnect()
     .withUrl("/hub/view")
     .build();
 
@@ -13,37 +12,13 @@ connection.on("viewCountUpdate", (value: number) => {
     counter.innerText = value.toString();
 });
 
-// notify server we're watching
-function notify() {
-    connection.send("notifyWatching");
-}
 
 // start the connection
 function startSuccess() {
     console.log("Connected.");
-    notify();
 }
 function startFail() {
     console.log("Connection failed.");
 }
 
 connection.start().then(startSuccess, startFail);
-
-// *** CONNECTION EVENTS ***
-let body = document.getElementsByTagName("body")[0];
-
-connection.onreconnected((connectionId: string) => {
-    body.style.backgroundColor = "green";
-
-    setTimeout(() => {
-        body.style.backgroundColor = "white";
-    }, 5000);
-});
-
-connection.onreconnecting((error: Error) => {
-    body.style.backgroundColor = "yellow";
-});
-
-connection.onclose((error: Error) => {
-    body.style.backgroundColor = "red";
-});
