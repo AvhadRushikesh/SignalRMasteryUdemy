9e57df52b6f86cc2f50b1924b1297d7187f190ec
rushi-smb <rushi@smbios.co.in>
2023-03-22 13:17:30 +0530

    Add Azure SignalR Service

--- SignalRMasteryUdemy/Startup.cs
+++ SignalRMasteryUdemy/Startup.cs
@@ -8,7 +8,12 @@ namespace SignalRMasteryUdemy
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR();
+            //  Go to Azure Service portal copy connection string and paste here
+            var connectionString = "";
+
+            /*  When WebSocket Connects it will not connect to localhost,
+                It will connect to Azure signalR service */
+            services.AddSignalR().AddAzureSignalR(connectionString);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
